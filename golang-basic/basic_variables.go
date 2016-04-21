2c4f0010fe0bc9d7bd719415ad364d16f4dddb0c
clx <brianchen.clx@gmail.com>
2016-04-21 19:23:06 +0800

    variables
    variables init and assifnment

--- golang-basic/basic_variables.go
+++ golang-basic/basic_variables.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	// variables declare
 	var v1 int
 	var v2 string
@@ -18,4 +17,23 @@ func main() {
 	var v6 *int           // pointer
 	var v7 map[string]int // map key type:string, value type:int
 	var v8 func(a int) int
+
+	var (
+		v9  int
+		v10 string
+	)
+
+	// variables init
+	var v11 int = 10
+	var v12 int
+	v12 = 10
+	var v13 = 12
+	v14 := 14
+
+	// variables assignment
+	var v15 int
+	v15 = 15
+	v16, v17 := 16, 17
+	fmt.Println(v16)
+	fmt.Println(v17)
 }

ea4719ff3d2a1a8ad1e2828d66adf91f61f79112
clx <brianchen.clx@gmail.com>
2016-05-17 00:51:38 +0800

    commit var practice

--- golang-basic/basic_variables.go
+++ golang-basic/basic_variables.go
@@ -7,33 +7,45 @@ import (
 
 func main() {
 	// variables declare
-	var v1 int
-	var v2 string
-	var v3 [10]int // array
-	var v4 []int   // slice
-	var v5 struct {
-		f int
-	}
-	var v6 *int           // pointer
-	var v7 map[string]int // map key type:string, value type:int
-	var v8 func(a int) int
+	//	var v1 int
+	//	var v2 string
+	//	var v3 [10]int // array
+	//	var v4 []int   // slice
+	//	var v5 struct {
+	//		f int
+	//	}
+	//	var v6 *int           // pointer
+	//	var v7 map[string]int // map key type:string, value type:int
+	//	var v8 func(a int) int
 
-	var (
-		v9  int
-		v10 string
-	)
+	//	var (
+	//		v9  int
+	//		v10 string
+	//	)
 
-	// variables init
-	var v11 int = 10
-	var v12 int
-	v12 = 10
-	var v13 = 12
-	v14 := 14
+	//	// variables init
+	//	var v11 int = 10
+	//	var v12 int
+	//	v12 = 10
+	//	var v13 = 12
+	//	v14 := 14
 
 	// variables assignment
-	var v15 int
-	v15 = 15
-	v16, v17 := 16, 17
-	fmt.Println(v16)
-	fmt.Println(v17)
+	//	var v15 int
+	//	v15 = 15
+	//	v16, v17 := 16, 17
+	//	fmt.Println(v16)
+	//	fmt.Println(v17)
+
+	// give variable value
+	var v18 int
+	v18 = 18
+	fmt.Println(v18)
+
+	// ananymous variables
+	//	func getName() (firstName, lastName string)  {
+	//		return "brian", "chen"
+	//	}
+
+	//	_,lastName :=getName()
 }

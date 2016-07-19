70d63264e4384b76ef0d69758825228ca37136c6
clx <brianchen.clx@gmail.com>
2016-07-19 22:31:08 +0800

    commit
    array&slice operate

--- golang-basic/basic_array.go
+++ golang-basic/basic_array.go
@@ -45,6 +45,41 @@ func main() {
 
 	// array slice
 	// base on array
+	//	var array [5]int = [5]int{0, 1, 2, 3, 4}
+	//	var mySlice []int = array[1:5]
+	//	for _, v := range array {
+	//		fmt.Println("ori array:", v)
+	//	}
+	//	for _, v := range mySlice {
+	//		fmt.Println("slice:", v)
+	//	}
 
 	// declare array slice directly
+	// 5 element
+	//	mySlice1 := make([]int, 5)
+	// 5 element & 10 cap
+	//	mySlice2 := make([]int, 5, 10)
+	// init directly
+	//	mySlice3 := []int{1, 2, 3, 4, 5}
+
+	// len() and cap() func
+	//	fmt.Println("len of mySlice2:", len(mySlice2))
+	//	fmt.Println("cap of mySlice2:", cap(mySlice2))
+
+	// append() func
+	//	mySlice3 = append(mySlice3, 6, 7, 8)
+	//	mySlice4 := []int{9, 10}
+	//	mySlice3 = append(mySlice3, mySlice4...)
+	//	fmt.Println(mySlice3)
+	// based on slice
+	//	newSlice := mySlice3[3:8]
+	//	fmt.Println(newSlice)
+
+	// copy() func
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := []int{6, 7, 8}
+	// copy(slice1, slice2)
+	copy(slice2, slice1)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }

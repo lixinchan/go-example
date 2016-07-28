6d6ae4b8f21c497b9c564c8aa9b64d17f6c4852b
clx <brianchen.clx@gmail.com>
2016-07-28 10:01:15 +0800

    commit
    function operate

--- golang-basic/basic_func.go
+++ golang-basic/basic_func.go
@@ -6,5 +6,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// 匿名函数&闭包
+	var j int = 5
+	a := func() func() {
+		var i int = 10
+		return func() {
+			fmt.Println("i, j: %d, %d\n", i, j)
+		}
+	}()
+	a()
+	j *= 2
+	a()
 }

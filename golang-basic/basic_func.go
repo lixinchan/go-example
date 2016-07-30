9fe3e962a0e7015ab2a8bc7f3fbe0865f3e76708
clx <brianchen.clx@gmail.com>
2016-07-30 09:27:25 +0800

    commit
    func's var

--- golang-basic/basic_func.go
+++ golang-basic/basic_func.go
@@ -5,6 +5,21 @@ import (
 	"fmt"
 )
 
+func MyPrint(args ...interface{}) {
+	for _, arg := range args {
+		switch arg.(type) {
+		case int:
+			fmt.Println(arg, "it's int")
+		case string:
+			fmt.Println(arg, "it's string")
+		case float64:
+			fmt.Println(arg, "it's float64")
+		default:
+			fmt.Println(arg, "it's unknown type")
+		}
+	}
+}
+
 func main() {
 
 	// 匿名函数&闭包
@@ -18,4 +33,12 @@ func main() {
 	a()
 	j *= 2
 	a()
+
+	// 不定参数
+	var v1 int = 3
+	var v2 string = "hello"
+	var v3 float64 = 4.5
+	var v4 complex64 = complex(3, 3.5)
+	MyPrint(v1, v2, v3, v4)
+
 }

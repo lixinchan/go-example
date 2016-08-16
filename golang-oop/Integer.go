1047c0cd97a7e45f3777459f6620850a919434d3
clx <brianchen.clx@gmail.com>
2016-08-16 23:44:34 +0800

    commit
    oop & po

--- golang-oop/Integer.go
+++ golang-oop/Integer.go
@@ -7,7 +7,13 @@ import (
 
 type Integer int
 
+// object oriented programing
 func (a Integer) Less(b Integer) bool {
+	return a < b
+}
+
+// processing oriented programing
+func Integer_Less(a, b Integer) bool {
 	return a < b
 }
 
@@ -15,4 +21,7 @@ func main() {
 	var a Integer = 3
 	val := a.Less(5)
 	fmt.Println(val)
+	fmt.Println("---")
+	var ret bool = Integer_Less(5, 3)
+	fmt.Println(ret)
 }

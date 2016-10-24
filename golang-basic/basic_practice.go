073637bcfa520aa47cf51d77ffd4fa662c69e59b
clx <brianchen.clx@gmail.com>
2016-10-24 18:21:39 +0800

    commit
    practice

--- golang-basic/basic_practice.go
+++ golang-basic/basic_practice.go
@@ -49,8 +49,15 @@ func main() {
 
 	// := 什么时候可用？本次声明的变量与index在同一作用于中，如果index在外层已经声明，
 	//	那么新声明的这个变量将是一个新的变量，并且，类型需要一致，本次声明中至少有一个变量是新声明的
-	var index int = 5
-	idx, index := 1, 3
-	fmt.Println(idx, index)
+	//	var index int = 5
+	//	idx, index := 1, 3
+	//	fmt.Println(idx, index)
+
+	// ++, -- 是语句而不是表达式，
+	array := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i, j := 0, len(array)-1; i < j; i, j = i+1, j-1 {
+		array[i], array[j] = array[j], array[i]
+	}
+	fmt.Println(array)
 
 }

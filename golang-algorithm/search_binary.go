a8650aa55b2b9be90221cd27c2fcaef9e8b0cd80
brianchen.clx <brianchen.clx@gmail.com>
2016-12-07 10:43:57 +0800

    commit
    add binary search by cycle&recursion

--- golang-algorithm/search_binary.go
+++ golang-algorithm/search_binary.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func search(array []int, key int) int {
+func search_cycle(array []int, key int) int {
 	low := 0
 	high := len(array) - 1
 	for low <= high {
@@ -21,8 +21,23 @@ func search(array []int, key int) int {
 	return -1
 }
 
+func search_recursion(array []int, key int, low int, high int) int {
+	if low > high {
+		return -1
+	}
+	mid := low + (high-low)/2
+	if key > array[mid] {
+		return search_recursion(array, key, mid+1, high)
+	} else if key < array[mid] {
+		return search_recursion(array, key, low, mid-1)
+	} else {
+		return mid
+	}
+}
+
 func main() {
 	var array []int = []int{1, 2, 3, 4, 5, 6, 7}
-	retVal := search(array, 3)
+	retVal := search_cycle(array, 3)
 	fmt.Println(retVal)
+	fmt.Println(search_recursion(array, 4, 1, 7))
 }

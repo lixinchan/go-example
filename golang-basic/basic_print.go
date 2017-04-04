e416dac7ad801ea16fe5c0f17cdd7b7048934688
clx <brianchen.clx@gmail.com>
2017-04-04 22:59:54 +0800

    print

--- golang-basic/basic_print.go
+++ golang-basic/basic_print.go
@@ -6,6 +6,16 @@ import (
 	"os"
 )
 
+type T struct {
+	a int
+	b float64
+	c string
+}
+
+// 控制自定义类型的默认格式
+func (t *T) String() string {
+	return fmt.Sprintf("%d%g%q", t.a, t.b, t.c)
+}
 func main() {
 
 	fmt.Printf("Hello %d\n", 23)
@@ -20,22 +30,36 @@ func main() {
 
 	fmt.Println("----------")
 
-	type T struct {
-		a int
-		b float64
-		c string
-	}
-
 	var timeZone = map[string]int{"UTC": 0 * 60 * 60, "CST": -6 * 60 * 60}
 
 	t := &T{7, -2.35, "abc\tdef"}
+	// pring value
 	fmt.Printf("%v\n", t)
+	// 为结构体添加字段名称
 	fmt.Printf("%+v\n", t)
+	// 按照go的语法打印值
 	fmt.Printf("%#v\n", t)
 	fmt.Printf("%#v\n", timeZone)
 
 	fmt.Println("----------")
 
+	//print type
 	fmt.Printf("%T\n", timeZone)
 
+	fmt.Println("----------")
+
+	// 遇到string/[]byte值时，产生带引号的字符串
+	str := "hello, go"
+	array := []byte{1, 2, 3, 4, 5}
+	fmt.Printf("%q\n", str)
+	fmt.Printf("%#q\n", array)
+
+	fmt.Println("----------")
+
+	// 控制自定义类型的默认格式
+
+	fmt.Println(^0)
+	fmt.Println(uint(0))
+	fmt.Println(^uint(0))
+	fmt.Println(^uint(0) >> 1)
 }

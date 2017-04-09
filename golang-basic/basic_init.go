aca8f82d8d4f6a771122bb99310db17a592cd2de
clx <brianchen.clx@gmail.com>
2017-04-09 13:10:26 +0800

    variables init

--- golang-basic/basic_init.go
+++ golang-basic/basic_init.go
@@ -4,19 +4,56 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+// enum
+type ByteSize float64
+
+const (
+	_           = iota
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+)
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
 func main() {
-	// enum
-	type ByteSize float64
-	const (
-		KB ByteSize = 1 << (10 * iota)
-		MB
-		GB
-		TB
-		PB
-		EB
-	)
+
 	fmt.Println(KB)
+	fmt.Println(MB)
 	fmt.Println(GB)
+
+	//variables
+	var (
+		home   = os.Getenv("HOME")
+		user   = os.Getenv("USER")
+		gopath = os.Getenv("GOPATH")
+	)
+	fmt.Println(home)
+	fmt.Println(user)
+	fmt.Println(gopath)
+
+	// init function
+
 }

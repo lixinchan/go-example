b5635dc984a106374ffcc381077e672d2c12e7e4
brianchen.clx <brianchen.clx@gmail.com>
2017-06-28 14:46:06 +0800

    add search the key last present index

--- golang-algorithm/search_binary.go
+++ golang-algorithm/search_binary.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// binary search cycle impl
 func search_cycle(array []int, key int) int {
 	low := 0
 	high := len(array) - 1
@@ -21,6 +22,7 @@ func search_cycle(array []int, key int) int {
 	return -1
 }
 
+// binary search recursion impl
 func search_recursion(array []int, key int, low int, high int) int {
 	if low > high {
 		return -1
@@ -35,9 +37,36 @@ func search_recursion(array []int, key int, low int, high int) int {
 	}
 }
 
+// search the key last present index
+func search_last_index(array []int, key int) int {
+	if array == nil {
+		return -1
+	}
+	low := 0
+	high := len(array) - 1
+	for low < high {
+		mid := low + ((high - low) >> 1) + ((high - low) & 1)
+		fmt.Println(mid)
+		if array[mid] > key {
+			high = mid - 1
+		} else {
+			low = mid
+		}
+	}
+
+	if array[low] == key {
+		return low
+	} else {
+		return -1
+	}
+	return -1
+}
+
 func main() {
-	var array []int = []int{1, 2, 3, 4, 5, 6, 7}
-	retVal := search_cycle(array, 3)
-	fmt.Println(retVal)
-	fmt.Println(search_recursion(array, 4, 1, 7))
+	var array []int = []int{1, 2, 3, 4, 5, 5, 6, 7, 8}
+	//	retVal := search_cycle(array, 3)
+	//	fmt.Println(retVal)
+	//	fmt.Println(search_recursion(array, 4, 1, 7))
+	retVal := search_last_index(array, 5)
+	fmt.Println("ret:", retVal)
 }

88d3cbc270b8f4053cf631b65bf42be35146274f
clx <brianchen.clx@gmail.com>
2018-03-20 09:30:57 +0800

    fixed bug

--- golang-algorithm/src/chapterone/search_binary.go
+++ golang-algorithm/src/chapterone/search_binary.go
@@ -6,7 +6,11 @@ import (
 )
 
 // binary search cycle impl
-func search_cycle(array []int, key int) int {
+func searchCycle(array []int, key int) int {
+	if array == nil || len(array) == 0 {
+		return -1
+	}
+
 	low := 0
 	high := len(array) - 1
 	for low <= high {
@@ -23,30 +27,32 @@ func search_cycle(array []int, key int) int {
 }
 
 // binary search recursion impl
-func search_recursion(array []int, key int, low int, high int) int {
+func searchRecursion(array []int, key int, low int, high int) int {
+	if array == nil || len(array) == 0 {
+		return -1
+	}
 	if low > high {
 		return -1
 	}
 	mid := low + (high-low)/2
 	if key > array[mid] {
-		return search_recursion(array, key, mid+1, high)
+		return searchRecursion(array, key, mid+1, high)
 	} else if key < array[mid] {
-		return search_recursion(array, key, low, mid-1)
+		return searchRecursion(array, key, low, mid-1)
 	} else {
 		return mid
 	}
 }
 
 // search the key last present index
-func search_last_index(array []int, key int) int {
-	if array == nil {
+func searchLastIndex(array []int, key int) int {
+	if array == nil || len(array) == 0 {
 		return -1
 	}
 	low := 0
 	high := len(array) - 1
 	for low < high {
 		mid := low + ((high - low) >> 1) + ((high - low) & 1)
-		fmt.Println(mid)
 		if array[mid] > key {
 			high = mid - 1
 		} else {
@@ -63,10 +69,10 @@ func search_last_index(array []int, key int) int {
 }
 
 func main() {
-	var array []int = []int{1, 2, 3, 4, 5, 5, 6, 7, 8}
-	//	retVal := search_cycle(array, 3)
-	//	fmt.Println(retVal)
-	//	fmt.Println(search_recursion(array, 4, 1, 7))
-	retVal := search_last_index(array, 5)
+	array := []int{1, 2, 3, 4, 5, 5, 6, 7, 8}
+	retVal := searchCycle(array, 3)
+	fmt.Println(retVal)
+	fmt.Println(searchRecursion(array, 4, 1, 7))
+	retVal = searchLastIndex(array, 5)
 	fmt.Println("ret:", retVal)
 }

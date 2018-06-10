a8f87dd9051262bbbe52ad084b4e28d3f705739c
clx <brianchen.clx@gmail.com>
2018-06-10 18:23:57 +0800

    variables

---
+++ golang-basic/src/reference/short-variable-declarations.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j = 1, 2
+	c := 3
+	python := false
+	java := "hello"
+	fmt.Println(i, j, c, python, java)
+	fmt.Println()
+}
---
+++ golang-basic/src/reference/variables-with-initializer.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, 3, "hello!"
+	fmt.Println(i, j, c, python, java)
+}

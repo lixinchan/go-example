f47f55b5b788ae490762252ee42847712178f8ba
clx <brianchen.clx@gmail.com>
2018-07-05 17:45:32 +0800

    exercise

--- golang-basic/src/reference/exercise-rot-reader.go
+++ golang-basic/src/reference/exercise-rot-reader.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"io"
-	"strings"
-	"os"
+	"strconv"
+	"fmt"
 )
 
 type rot13Reader struct {
 	reader io.Reader
 }
 
+// init rot13
+func initROT13() (rotMap map[string]string) {
+	rotMap = make(map[string]string)
+	var idx int
+	var count int = 0
+	for idx = 'a'; idx <= 'z'; idx++ {
+		if count < 13 {
+			rotMap[strconv.Itoa(idx)] = strconv.Itoa(idx + 13)
+		} else {
+			rotMap[strconv.Itoa(idx)] = strconv.Itoa(idx - 13)
+		}
+		count++
+	}
+	count = 0
+	for idx = 'A'; idx <= 'Z'; idx++ {
+		if count < 13 {
+			rotMap[strconv.Itoa(idx)] = strconv.Itoa(idx + 13)
+		} else {
+			rotMap[strconv.Itoa(idx)] = strconv.Itoa(idx - 13)
+		}
+		count++
+	}
+	return rotMap
+}
+
 func (reader *rot13Reader) Read(b []byte) (n int, err error) {
 
+	return 0, nil
 }
 
 func main() {
-	str := strings.NewReader("clx")
-	reader := rot13Reader{str}
-	io.Copy(os.Stdout, &reader)
+	//str := strings.NewReader("clx")
+	//reader := rot13Reader{str}
+	//io.Copy(os.Stdout, &reader)
+
+	rotMap := initROT13()
+	for key, val := range rotMap {
+		fmt.Println(key, val)
+	}
 }

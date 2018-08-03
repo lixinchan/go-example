54967306e514650e3af12c9f817e8b49fc7917b9
clx <brianchen.clx@gmail.com>
2018-08-03 08:01:43 +0800

    quicksort

--- golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort.go
+++ golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort.go
@@ -1,6 +1,21 @@
 package forthchapter
 
 // quick sort
-func QuickSort(n int) {
+func QuickSort(array []int) []int {
+	if array == nil {
+		return nil
+	}
+	if len(array) < 2 {
+		return array
+	} else {
+		pivot := array[0]
+		for val := range array {
+			if val <= pivot {
 
+			} else {
+
+			}
+		}
+		return nil
+	}
 }

36990fc46dc5b4dd881694fab8dce302ddff12ab
clx <brianchen.clx@gmail.com>
2018-08-08 17:54:52 +0800

    add map equal method

--- golang-basic/src/reference/maps.go
+++ golang-basic/src/reference/maps.go
@@ -8,6 +8,19 @@ type Vertex struct {
 
 var m map[string]Vertex
 
+// equal two map
+func equal(x, y map[int]string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for key, xVal := range x {
+		if yVal, ok := y[key]; !ok || xVal != yVal {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	m = make(map[string]Vertex)
 	m["test-map"] = Vertex{

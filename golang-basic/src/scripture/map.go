6ad94ad5e03cc326d6a49e8475adc314de7de5b9
clx <brianchen.clx@gmail.com>
2018-08-09 00:09:26 +0800

    map test

--- golang-basic/src/scripture/map.go
+++ golang-basic/src/scripture/map.go
@@ -1,6 +1,21 @@
 package scripture
 
+import (
+	"sort"
+	"fmt"
+)
+
 // sort map by key
 func SortMap(ages map[string]int) {
-	
+	if ages == nil {
+		return
+	}
+	names := make([]string, 0, len(ages))
+	for name := range ages {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Printf("%s\t%d\n", name, ages[name])
+	}
 }
--- golang-basic/src/scripture/map_test.go
+++ golang-basic/src/scripture/map_test.go
@@ -1,7 +1,15 @@
 package scripture_test
 
-import "testing"
+import (
+	. "scripture"
+	"testing"
+)
 
 func TestSortMap(t *testing.T) {
-
+	ages := map[string]int{
+		"lmm": 28,
+		"abb": 1,
+		"clx": 27,
+	}
+	SortMap(ages)
 }

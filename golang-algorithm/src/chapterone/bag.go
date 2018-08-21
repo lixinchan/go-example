49b4c6693fb6ef05c381f3ad7e7b3ddc65b7b7fd
clx <brianchen.clx@gmail.com>
2018-08-21 13:04:43 +0800

    add bag

--- golang-algorithm/src/chapterone/bag.go
+++ golang-algorithm/src/chapterone/bag.go
@@ -1,1 +1,37 @@
 package chapterone
+
+type Bag interface {
+	Size() int
+	IsEmpty() bool
+	Add()
+}
+
+// type node
+type node struct {
+	data interface{}
+	next *node
+}
+
+var (
+	first  node
+	length int
+)
+
+// add
+func Add(data interface{}) {
+	oldFirst := first
+	first = node{}
+	first.data = data
+	first.next = &oldFirst
+	length++
+}
+
+// size
+func Size() int {
+	return length
+}
+
+// isEmpty
+func IsEmpty() bool {
+	return length == 0
+}
--- golang-algorithm/src/chapterone/bag_test.go
+++ golang-algorithm/src/chapterone/bag_test.go
@@ -1,1 +1,14 @@
 package chapterone_test
+
+import (
+	"testing"
+	. "chapterone"
+)
+
+func TestAdd(t *testing.T) {
+	t.Log(IsEmpty())
+	for idx:=10; idx >0;idx-- {
+		Add(idx)
+	}
+	t.Log(Size())
+}

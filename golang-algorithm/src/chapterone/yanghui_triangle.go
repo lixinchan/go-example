412119bac60f977278db1ef74ac09e8fcba0daca
clx <brianchen.clx@gmail.com>
2018-09-09 23:01:18 +0800

    add yanghui triangle

---
+++ golang-algorithm/src/chapterone/yanghui_triangle.go
@@ -0,0 +1,25 @@
+package chapterone
+
+import "fmt"
+
+// print yanghui triangle
+func PrintTriangle(line int) {
+	array := make([]int, 10)
+	for idx := 0; idx < line; idx++ {
+		for jdx := 0; jdx < (line - idx); jdx++ {
+			fmt.Print("-")
+		}
+		for jdx := 0; jdx < (idx + 1); jdx++ {
+			length := len(array)
+			var value int
+			if jdx == 0 || jdx == idx {
+				value = 1
+			} else {
+				value = array[length-idx] + array[length-idx-1]
+			}
+			array = append(array, value)
+			fmt.Print(value, " ")
+		}
+		fmt.Println()
+	}
+}
---
+++ golang-algorithm/src/chapterone/yanghui_triangle_test.go
@@ -0,0 +1,10 @@
+package chapterone_test
+
+import (
+	"testing"
+	. "chapterone"
+)
+
+func TestPrintTriangle(t *testing.T) {
+	PrintTriangle(9)
+}

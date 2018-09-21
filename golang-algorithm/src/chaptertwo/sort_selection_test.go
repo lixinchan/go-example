9cd1cd2cf5a23f454b54e12493e94a91b4d94403
clx <brianchen.clx@gmail.com>
2018-09-21 10:13:25 +0800

    add selection sort

--- golang-algorithm/src/chaptertwo/sort_selection_test.go
+++ golang-algorithm/src/chaptertwo/sort_selection_test.go
@@ -11,3 +11,10 @@ func TestSelectionSort(t *testing.T) {
 	SelectionSort(array)
 	PrintArray(array)
 }
+
+func TestSelectionSortEnhance(t *testing.T) {
+	array := []int{5, 7, 6, 1, 4, 3, 2, 0}
+	PrintArray(array)
+	SelectionSortEnhance(array)
+	PrintArray(array)
+}

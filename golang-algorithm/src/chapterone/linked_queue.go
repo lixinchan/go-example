fc33b10cd1c2ae7b015976630b24405a6755b6d6
clx <brianchen.clx@gmail.com>
2018-09-26 07:58:22 +0800

    add linked queue

--- golang-algorithm/src/chapterone/linked_queue.go
+++ golang-algorithm/src/chapterone/linked_queue.go
@@ -1,1 +1,37 @@
+// FIFO QUEUE
 package chapterone
+
+type LinkedQueueNode struct {
+	next *LinkedStackNode
+	data interface{}
+}
+
+type LinkedQueue interface {
+	LinkedEnqueue(data interface{})
+	LinkedDequeue() (interface{}, error)
+	LinkedIsEmpty() bool
+	LinkedSize() int
+}
+
+var (
+	linkedSize   int
+	linkedHeader *LinkedQueueNode
+	linkedTail   *LinkedQueueNode
+)
+
+func (node *LinkedQueueNode) LinkedEnqueue(data interface{}) {
+
+}
+
+func (node *LinkedQueueNode) LinkedDequeue() (interface{}, error) {
+
+	return nil, nil
+}
+
+func (node LinkedQueueNode) LinkedIsEmpty() bool {
+	return linkedSize == 0
+}
+
+func (node LinkedQueueNode) LinkedSize() int {
+	return linkedSize
+}

bb1f6066c3af89adaa58ff75da5cc4fc288821fa
clx <brianchen.clx@gmail.com>
2018-09-27 14:35:20 +0800

    add linked queue

--- golang-algorithm/src/chapterone/linked_queue_test.go
+++ golang-algorithm/src/chapterone/linked_queue_test.go
@@ -1,1 +1,19 @@
 package chapterone_test
+
+import (
+	. "chapterone"
+	"testing"
+)
+
+func TestLinkedQueueNode_LinkedEnquque(t *testing.T) {
+	var queue LinkedQueue
+	queue = InitLinkedQueue()
+	for idx := 0; idx < 10; idx++ {
+		queue.LinkedEnqueue(idx)
+	}
+	t.Log(queue.LinkedSize())
+	for !queue.LinkedIsEmpty() {
+		t.Log(queue.LinkedDequeue())
+	}
+
+}

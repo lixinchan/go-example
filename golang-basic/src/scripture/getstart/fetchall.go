cb14ee659c6c25c89355fd59f483284fe2d66bc0
clx <brianchen.clx@gmail.com>
2018-10-08 14:47:42 +0800

    add fetch test

--- golang-basic/src/scripture/getstart/fetchall.go
+++ golang-basic/src/scripture/getstart/fetchall.go
@@ -1,26 +1,27 @@
 package getstart
 
 import (
-	"time"
-	"net/http"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-func fetch(url string, ch chan<- string) {
+// fetch
+func Fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		ch <- fmt.Sprint(err)
 		return
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	bytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
 	seconds := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", seconds, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs %7d %s", seconds, bytes, url)
 }
--- golang-basic/src/scripture/getstart/fetchall_test.go
+++ golang-basic/src/scripture/getstart/fetchall_test.go
@@ -1,7 +1,20 @@
 package getstart
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
 
 func TestFetch(t *testing.T) {
-
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go Fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }

4991003acce13dc8de55d5dbe3d245a21e409669
clx <brianchen.clx@gmail.com>
2019-02-27 02:28:40 +0800

    change quicksort

--- golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort.go
+++ golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort.go
@@ -1,25 +1,51 @@
 package forthchapter
 
+import "grokkingalgorithms/common"
+
 // quick sort
 func QuickSort(array []int) []int {
 	if array == nil {
 		return nil
 	}
-	if len(array) < 2 {
-		return array
-	} else {
-		pivot := array[0]
-		var less = []int{}
-		var grater = []int{}
-		for _, val := range array[1:] {
-			if val <= pivot {
-				less = append(less, val)
-			} else {
-				grater = append(grater, val)
+	if len(array) == 0 {
+		return nil
+	}
+	sort(array, 0, len(array)-1)
+	return array
+}
+
+func sort(array []int, lo, hi int) {
+	if hi <= lo {
+		return
+	}
+	// partition array
+	idx := partition(array, lo, hi)
+	// sort left
+	sort(array, lo, idx-1)
+	// sort right
+	sort(array, idx+1, hi)
+}
+
+func partition(array []int, lo, hi int) int {
+	idx := lo + 1
+	jdx := hi
+	pivot := array[lo]
+	for true {
+		for ; common.Less(array[idx], pivot); idx++ {
+			if idx == hi {
+				break
 			}
 		}
-		less = append(QuickSort(less), pivot)
-		grater = QuickSort(grater)
-		return append(less, grater...)
+		for ; common.Less(pivot, array[jdx]); jdx-- {
+			if jdx == lo {
+				break
+			}
+		}
+		if idx >= jdx {
+			break
+		}
+		common.Exchange(array, idx, jdx)
 	}
+	common.Exchange(array, lo, jdx)
+	return jdx
 }
--- golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort_test.go
+++ golang-algorithm/src/grokkingalgorithms/forthchapter/quick_sort_test.go
@@ -1,14 +1,15 @@
 package forthchapter
 
 import (
-	"testing"
 	"grokkingalgorithms/common"
+	"testing"
 )
 
 func TestQuickSort(t *testing.T) {
 	array := []int{1, 2, 7, 4, 5, 10, 6, 9, 8, 7, 3}
 	common.PrintArray(array)
-	t.Log(QuickSort(array))
+	QuickSort(array)
+	common.PrintArray(array)
 }
 
 func BenchmarkQuickSort(b *testing.B) {
